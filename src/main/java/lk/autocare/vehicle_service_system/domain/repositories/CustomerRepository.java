b62052cd98791474dce4ee641a0c2808ebc76e87
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-17 11:49:45 +0530

    feat: initiate update customer method

--- src/main/java/lk/autocare/vehicle_service_system/domain/repositories/CustomerRepository.java
+++ src/main/java/lk/autocare/vehicle_service_system/domain/repositories/CustomerRepository.java
@@ -11,4 +11,7 @@ public interface CustomerRepository {
 
     //save new customer
     void saveCustomer(Customer customer);
+
+    //update customer
+    void updateCustomer(Long customerId, Customer customer);
 }
