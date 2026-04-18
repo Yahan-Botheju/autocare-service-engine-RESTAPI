69f453c47c726dfd5fc5a92357867705240de542
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-18 19:47:28 +0530

    faet: initiate find by id method

--- src/main/java/lk/autocare/vehicle_service_system/domain/repositories/VehicleRepository.java
+++ src/main/java/lk/autocare/vehicle_service_system/domain/repositories/VehicleRepository.java
@@ -1,4 +1,11 @@
 package lk.autocare.vehicle_service_system.domain.repositories;
 
+import lk.autocare.vehicle_service_system.domain.models.Vehicle;
+
+import java.util.Optional;
+
 public interface VehicleRepository {
+
+    //vehicle find by id
+    Optional<Vehicle> findById(Long vehicleId);
 }
