307e1272af2444904d9d267da5dc5bcd87840c3c
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-18 11:54:38 +0530

    feat: setup persistence mapper

--- src/main/java/lk/autocare/vehicle_service_system/infrastructure/vehicle/persistence/mapper/VehiclePersistenceMapper.java
+++ src/main/java/lk/autocare/vehicle_service_system/infrastructure/vehicle/persistence/mapper/VehiclePersistenceMapper.java
@@ -1,4 +1,20 @@
 package lk.autocare.vehicle_service_system.infrastructure.vehicle.persistence.mapper;
 
+import lk.autocare.vehicle_service_system.domain.models.Vehicle;
+import lk.autocare.vehicle_service_system.infrastructure.vehicle.persistence.entity.VehicleEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
 public interface VehiclePersistenceMapper {
+
+    //domain model to entity
+    VehicleEntity toEntity(Vehicle vehicle);
+
+    //entity to domain model
+    Vehicle toDomainModel(VehicleEntity vehicleEntity);
+
+    //old entity to new domain model edited data
+    VehicleEntity oldEntityDataToNewEntity(Vehicle vehicle ,@MappingTarget  VehicleEntity vehicleEntity);
+
 }
