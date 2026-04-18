4a8eae79b8a511e3b45544ca152ce412effd1f68
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-18 12:08:36 +0530

    fix: implement contrast, mistakenly inject domain repo instaed of jpa repo

--- src/main/java/lk/autocare/vehicle_service_system/infrastructure/vehicle/persistence/VehicleRepositoryImpl.java
+++ src/main/java/lk/autocare/vehicle_service_system/infrastructure/vehicle/persistence/VehicleRepositoryImpl.java
@@ -1,16 +1,17 @@
 package lk.autocare.vehicle_service_system.infrastructure.vehicle.persistence;
 
 import lk.autocare.vehicle_service_system.domain.repositories.VehicleRepository;
+import lk.autocare.vehicle_service_system.infrastructure.vehicle.persistence.jpa.JpaVehicleRepository;
 import lk.autocare.vehicle_service_system.infrastructure.vehicle.persistence.mapper.VehiclePersistenceMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
-public class VehicleRepositoryImpl {
+public class VehicleRepositoryImpl implements  VehicleRepository {
 
     //inject domain repo
-    private final VehicleRepository vehicleRepository;
+    private final JpaVehicleRepository jpaVehicleRepository;
 
     //inject persistence mapper
     private final VehiclePersistenceMapper  vehiclePersistenceMapper;
