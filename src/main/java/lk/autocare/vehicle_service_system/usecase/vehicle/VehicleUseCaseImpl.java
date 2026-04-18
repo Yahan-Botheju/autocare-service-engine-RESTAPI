e13945d9f93e402b8faf4e713122fe22fa85b9b4
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-18 11:54:00 +0530

    feat: initiate UseCaseImpl as bean

--- src/main/java/lk/autocare/vehicle_service_system/usecase/vehicle/VehicleUseCaseImpl.java
+++ src/main/java/lk/autocare/vehicle_service_system/usecase/vehicle/VehicleUseCaseImpl.java
@@ -1,4 +1,11 @@
 package lk.autocare.vehicle_service_system.usecase.vehicle;
 
-public class VehicleUseCaseImpl {
+import lk.autocare.vehicle_service_system.domain.repositories.VehicleRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class VehicleUseCaseImpl implements  VehicleUseCase{
+
+    //inject domain repo as bean this class
+    private final VehicleRepository vehicleRepository;
 }
