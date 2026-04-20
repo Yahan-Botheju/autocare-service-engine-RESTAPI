bdcb625a85127146fd70b266f43fa436561c7bce
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-20 11:15:22 +0530

    upgrade: remove @repo, tight cppling and setup persistence bean config

---
+++ src/main/java/lk/autocare/vehicle_service_system/infrastructure/customer/config/CustomerPersistenceBeanConfig.java
@@ -0,0 +1,19 @@
+package lk.autocare.vehicle_service_system.infrastructure.customer.config;
+
+import lk.autocare.vehicle_service_system.domain.repositories.CustomerRepository;
+import lk.autocare.vehicle_service_system.infrastructure.customer.persistence.CustomerRepositoryImpl;
+import lk.autocare.vehicle_service_system.infrastructure.customer.persistence.jpa.JpaCustomerRepository;
+import lk.autocare.vehicle_service_system.infrastructure.customer.persistence.mapper.CustomerPersistenceMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CustomerPersistenceBeanConfig {
+    @Bean
+    public CustomerRepository customerRepository(
+            JpaCustomerRepository jpaCustomerRepository,
+            CustomerPersistenceMapper customerPersistenceMapper
+    ) {
+        return new CustomerRepositoryImpl(jpaCustomerRepository, customerPersistenceMapper);
+    }
+}
