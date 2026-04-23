d3bc3cc3c7236ccfa64f8d96329b612cdb47d468
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-23 10:59:53 +0530

    feat: config customer interceptor and inject interceptor

--- src/main/java/lk/autocare/vehicle_service_system/web/customer/config/CustomerWebConfig.java
+++ src/main/java/lk/autocare/vehicle_service_system/web/customer/config/CustomerWebConfig.java
@@ -1,4 +1,14 @@
 package lk.autocare.vehicle_service_system.web.customer.config;
 
-public class CustomerWebConfig {
+import lk.autocare.vehicle_service_system.web.customer.interceptor.CustomerRoleBaseInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class CustomerWebConfig implements WebMvcConfigurer {
+
+    //inject role base interceptor
+    private final CustomerRoleBaseInterceptor customerRoleBaseInterceptor;
 }
