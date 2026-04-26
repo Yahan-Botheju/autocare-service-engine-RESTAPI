70e4aeb8af077113750227fbb2c42cdcdd058496
yahan-botheju <yahan.ravinga.git@gmail.com>
2026-04-26 18:32:16 +0530

    test: test vehicle find by id function

--- src/test/java/lk/autocare/vehicle_service_system/VehicleRepositoryImplTest.java
+++ src/test/java/lk/autocare/vehicle_service_system/VehicleRepositoryImplTest.java
@@ -1,12 +1,20 @@
 package lk.autocare.vehicle_service_system;
 
+import lk.autocare.vehicle_service_system.domain.models.Vehicle;
 import lk.autocare.vehicle_service_system.infrastructure.vehicle.persistence.VehicleRepositoryImpl;
+import lk.autocare.vehicle_service_system.infrastructure.vehicle.persistence.entity.VehicleEntity;
 import lk.autocare.vehicle_service_system.infrastructure.vehicle.persistence.jpa.JpaVehicleRepository;
 import lk.autocare.vehicle_service_system.infrastructure.vehicle.persistence.mapper.VehiclePersistenceMapper;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class VehicleRepositoryImplTest {
@@ -20,6 +28,22 @@ public class VehicleRepositoryImplTest {
     @InjectMocks
     private VehicleRepositoryImpl vehicleRepositoryImpl;
 
+    @Test
+    void shouldFindById(){
+        Long vehicleId = 1L;
+        VehicleEntity dummyVehicleEntity = new VehicleEntity();
+        Vehicle dummyVehicleDomain = new Vehicle();
+
+        when(jpaVehicleRepository.findById(vehicleId)).thenReturn(Optional.of(dummyVehicleEntity));
+
+        when(vehiclePersistenceMapper.toDomainModel(dummyVehicleEntity)).thenReturn(dummyVehicleDomain);
+
+        Optional<Vehicle> result = vehicleRepositoryImpl.findById(vehicleId);
+
+        assertTrue(result.isPresent());
+        assertEquals(dummyVehicleDomain, result.get());
+
+    }
 }
 
 
